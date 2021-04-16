570c8bd249eb64e7dfd54a0d0527b8740ad141f1
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-04-16 15:56:11 +0300

    Modals text area completed for the 4 first movies of each category

--- queries.js
+++ queries.js
@@ -28,9 +28,9 @@ async function requestCategoryMovies(categoryRequests){
     let sortedCategoryMovies = sortMovies(categoryMovies);
     return sortedCategoryMovies;}
 
-/* Function returning an array with the URLs of the movies from a category */
+/* Function returning an array with the URLs of the pictures of the movies from a category */
 
-function extractMoviesUrls(sortedCategoryMovies){
+function extractMoviesPicturesUrls(sortedCategoryMovies){
     return sortedCategoryMovies.map(x => x['image_url']);}
 
 /* Function filling a carousel with movies' pictures */
@@ -41,45 +41,66 @@ function fillCarousel(sortedCategoryUrls, carousel, carouselCategory){
     carousel.fillCategoryMovies(sortedCategoryUrls);
     carousel.fillMoviesList(carouselCategory);}
 
-/* Function executing extractMoviesUrls() function then fillCarousel() function */
+/* Function executing extractMoviesPicturesUrls() and fillCarousel() functions successively */
 
 function fillCarouselFromUrls(sortedCategoryMovies, carousel, carouselCategory){
-    let sortedCategoryUrls = extractMoviesUrls(sortedCategoryMovies);
+    let sortedCategoryUrls = extractMoviesPicturesUrls(sortedCategoryMovies);
     fillCarousel(sortedCategoryUrls, carousel, carouselCategory);}
 
-/* Functions to get the title, the picture and the description of the Best Rated Movie */
+/* Function to fill the text areas of a category's modals */
+
+async function fillCategoryModals(array, category){
+    let firstMovieUrl = array[0].url;
+    let secondMovieUrl = array[1].url;
+    let thirdMovieUrl = array[2].url;
+    let fourthMovieUrl = array[3].url;
+
+    let requestFirstMovie = await fetch(firstMovieUrl);
+    let requestSecondMovie = await fetch(secondMovieUrl);
+    let requestThirdMovie = await fetch(thirdMovieUrl);
+    let requestFourthMovie = await fetch(fourthMovieUrl);
+
+    let dataFirstMovie = await requestFirstMovie.json();
+    let dataSecondMovie = await requestSecondMovie.json();
+    let dataThirdMovie = await requestThirdMovie.json();
+    let dataFourthMovie = await requestFourthMovie.json();
+
+    let categoryModals = document.querySelectorAll(category + ' p');
+    let firstMovieModal = categoryModals[0];
+    let secondMovieModal = categoryModals[1];
+    let thirdMovieModal = categoryModals[2];
+    let fourthMovieModal = categoryModals[3];
+
+    fillModal(firstMovieModal, dataFirstMovie);
+    fillModal(secondMovieModal, dataSecondMovie);
+    fillModal(thirdMovieModal, dataThirdMovie);
+    fillModal(fourthMovieModal, dataFourthMovie);}
+
+/* Functions to get the title, the picture and the description of the best rated movie */
 
 function getBestMovieTitle(array){
-let bestMovieTitle = array[0].title;
-document.querySelector('section h2').innerHTML = bestMovieTitle;}
+    let bestMovieTitle = array[0].title;
+    document.querySelector('section h2').innerHTML = bestMovieTitle;}
 
 function getBestMoviePicture(array){
-let bestMoviePicture = document.querySelector('section img');
-bestMoviePicture.src = array[0];}
-
-function writeModal(element, data){
-    element.innerHTML = "Genres: " + data.genres + "<br />"
-    + "Release Date: " + data.date_published + "<br />" + "Rated: "
-    + data.rated + "<br />" + "Imdb score: " + data.imdb_score + "<br />"
-    + "Directed by: " + data.directors + "<br />" + "Actors: " + data.actors + "<br />"
-    + "Duration: " + data.duration + "<br />" + "Countries: " + data.countries + "<br />"
-    + "Box-Office Result: " + data.metascore + "<br />"
-    + "Description: " + data.long_description + "<br />";}
+    let bestMoviePicture = document.querySelector('section img');
+    bestMoviePicture.src = array[0];}
 
 async function getBestMovieDescription(array){
-let bestMovieUrl = array[0].url;
-let requestBestMovie = await fetch(bestMovieUrl);
-let dataBestMovie = await requestBestMovie.json();
-let descriptionBestMovie = dataBestMovie.description;
-document.getElementById('description').innerHTML = descriptionBestMovie;
-let bestMovieModal = document.querySelector('#modalBestMovie p');
-writeModal(bestMovieModal, dataBestMovie);
-}
+    let bestMovieUrl = array[0].url;
+    let requestBestMovie = await fetch(bestMovieUrl);
+    let dataBestMovie = await requestBestMovie.json();
+    let descriptionBestMovie = dataBestMovie.description;
+    document.getElementById('description').innerHTML = descriptionBestMovie;
+    let bestMovieModal = document.querySelector('#modalBestMovie p');
+    fillModal(bestMovieModal, dataBestMovie);}
+
+/* Function executing the three previous functions successively */
 
 function getBestMovieDetails(array1, array2){
-getBestMovieTitle(array1);
-getBestMoviePicture(array2);
-getBestMovieDescription(array1);}
+    getBestMovieTitle(array1);
+    getBestMoviePicture(array2);
+    getBestMovieDescription(array1);}
 
 /* Execution of the functions defined above for all categories */
 
@@ -89,9 +110,10 @@ let bestMoviesUrls = ["http://localhost:8000/api/v1/titles/?sort_by=-imdb_score,
 "http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score,-votes"];
 let requestsBestMovies = fetchCategoryMovies(bestMoviesUrls);
 requestCategoryMovies(requestsBestMovies).then(function(sortedBestMovies){
-let sortedBestMoviesUrls = extractMoviesUrls(sortedBestMovies);
+let sortedBestMoviesUrls = extractMoviesPicturesUrls(sortedBestMovies);
 fillCarousel(sortedBestMoviesUrls, carousel1, "best-rated-movies");
-getBestMovieDetails(sortedBestMovies, sortedBestMoviesUrls);})
+getBestMovieDetails(sortedBestMovies, sortedBestMoviesUrls);
+fillCategoryModals(sortedBestMovies, '#best-rated-movies');})
 
 /* First Category Movies */
 
@@ -99,7 +121,8 @@ let firstCategoryUrls = ["http://localhost:8000/api/v1/titles/?&genre=animation&
 "http://localhost:8000/api/v1/titles/?page=2&genre=animation&sort_by=-imdb_score,-votes"];
 let requestsFirstCategory = fetchCategoryMovies(firstCategoryUrls);
 requestCategoryMovies(requestsFirstCategory).then(function(sortedFirstCategory){
-fillCarouselFromUrls(sortedFirstCategory, carousel2, "first-category");})
+fillCarouselFromUrls(sortedFirstCategory, carousel2, "first-category");
+fillCategoryModals(sortedFirstCategory, '#first-category');})
 
 /* Second Category Movies */
 
@@ -107,7 +130,8 @@ let secondCategoryUrls = ["http://localhost:8000/api/v1/titles/?&genre=drama&sor
 "http://localhost:8000/api/v1/titles/?page=2&genre=drama&sort_by=-imdb_score,-votes"];
 let requestsSecondCategory = fetchCategoryMovies(secondCategoryUrls);
 requestCategoryMovies(requestsSecondCategory).then(function(sortedSecondCategory){
-fillCarouselFromUrls(sortedSecondCategory, carousel3, "second-category");})
+fillCarouselFromUrls(sortedSecondCategory, carousel3, "second-category");
+fillCategoryModals(sortedSecondCategory, '#second-category');})
 
 /* Third Category Movies */
 
@@ -115,5 +139,5 @@ let thirdCategoryUrls = ["http://localhost:8000/api/v1/titles/?&genre=fantasy&so
 "http://localhost:8000/api/v1/titles/?page=2&genre=fantasy&sort_by=-imdb_score,-votes"];
 let requestsThirdCategory = fetchCategoryMovies(thirdCategoryUrls);
 requestCategoryMovies(requestsThirdCategory).then(function(sortedThirdCategory){
-fillCarouselFromUrls(sortedThirdCategory, carousel4, "third-category");})
-
+fillCarouselFromUrls(sortedThirdCategory, carousel4, "third-category");
+fillCategoryModals(sortedThirdCategory, '#third-category');})
