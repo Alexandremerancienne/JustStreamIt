e2e480b41f24a8a449e52b80391de865115fdf3f
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-04-16 16:41:59 +0300

    fillModalPictures() function added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+function fillModalText(element, data){
+    element.innerHTML = "<b>Title: </b>" + data.title + "<br />"
+    + "<b>Genres: </b>" + data.genres + "<br />"
+    + "<b>Release Date: </b>" + data.date_published + "<br />" + "<b>Rated: </b>"
+    + data.rated + "<br />" + "<b>Imdb score: </b>" + data.imdb_score + "<br />"
+    + "<b>Directed by: </b>" + data.directors + "<br />" + "<b>Actors: </b>" + data.actors + "<br />"
+    + "<b>Duration: </b>" + data.duration + "<br />" + "<b>Countries: </b>" + data.countries + "<br />"
+    + "<b>Box-Office Result: </b>" + data.metascore + "<br />" + "<br />" + "<br />"
+    + data.long_description + "<br />";}
+
+function fillModalPicture(img, data){
+    img.src = data.image_url;}
+
 /* Carousel class */
 
 class Carousel{
     constructor(category, pictures){
         this.category = category;
         this.pictures = pictures;
+        this.modals = [];
+        this.displayedModals = [];
         this.index = 0;
         this.categoryMovies = [];
         this.firstMovie = new Image();
@@ -16,6 +31,11 @@ class Carousel{
         let extendedPictures = pictures.concat(pictures, pictures, pictures);
         var i;
         for (i=0;i<7;i++){this.categoryMovies.push(extendedPictures.slice(4*i,4*i+4))};
+    }
+
+    fillCategoryModals(){
+        var j;
+        for (j=0;j<7;j++){this.displayedModals.push(this.modals.slice(4*i,4*i+4))};
     }
 
     fillMoviesList(movieCategory){
@@ -39,6 +59,8 @@ class Carousel{
         this.secondMovie.src = this.categoryMovies[this.index][1];
         this.thirdMovie.src = this.categoryMovies[this.index][2];
         this.fourthMovie.src = this.categoryMovies[this.index][3];
+        this.displayedModals = this.modals[index];
+
     }
 
     moveLeft(){
@@ -48,6 +70,7 @@ class Carousel{
         this.secondMovie.src = this.categoryMovies[this.index][1];
         this.thirdMovie.src = this.categoryMovies[this.index][2];
         this.fourthMovie.src = this.categoryMovies[this.index][3];
+        this.displayedModals = this.modals[index];
     }
 }
 
